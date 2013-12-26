36b08b88e20fbadc6e0f651e5aedb52934bbebd1
Isaac White <iaw2105@columbia.edu>
2013-12-26 13:37:35 -0400

    First working comparison of test dictionary

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-IW = {};
-require("./dictionary4.js");
+
+// require("./dictionary4.js");
+// var fs = require("fs");
+
 IW.cleanDictionary = [];
 for (var i = 0; i < IW.dictionary.length; i++) {
 	if(IW.dictionary[i] !== 1) {
@@ -32,48 +34,66 @@ function compareArrays(array2) {
 var correct = compareArrays(IW.cleanDictionary);
 console.log(correct);
 
-IW.stringConvert = "";
-var startIndex = 0;
-//compress the array;
-for (i=0; i< IW.cleanDictionary.length; i += 6) {
-	var ones,twos,fours,eights,sixteens,thirtytwos,charcode;
-	ones = IW.cleanDictionary[i];
-	twos = IW.cleanDictionary[i+1];
-	fours = IW.cleanDictionary[i+2];
-	eights = IW.cleanDictionary[i+3];
-	sixteens = IW.cleanDictionary[i+4];
-	thirtytwos = IW.cleanDictionary[i+5];
+// IW.stringConvert = "";
+// //compress the array;
+// for (i=0; i< IW.cleanDictionary.length; i += 6) {
+// 	var ones,twos,fours,eights,sixteens,thirtytwos,charcode;
+// 	ones = IW.cleanDictionary[i];
+// 	twos = IW.cleanDictionary[i+1];
+// 	fours = IW.cleanDictionary[i+2];
+// 	eights = IW.cleanDictionary[i+3];
+// 	sixteens = IW.cleanDictionary[i+4];
+// 	thirtytwos = IW.cleanDictionary[i+5];
 
-	charcode = 33; //avoid blank spaces
-	charcode += ones;
-	charcode += twos * 2;
-	charcode += fours * 4;
-	charcode += eights * 8;
-	charcode += sixteens * 16;
-	charcode += thirtytwos * 32;
+// 	charcode = 33; //avoid blank spaces
+// 	charcode += ones;
+// 	charcode += twos * 2;
+// 	charcode += fours * 4;
+// 	charcode += eights * 8;
+// 	charcode += sixteens * 16;
+// 	charcode += thirtytwos * 32;
 
-	IW.stringConvert += String.fromCharCode(charcode);
+// 	IW.stringConvert += String.fromCharCode(charcode);
+// }
+
+IW.extractArray = function(compressedString) {
+	var compressedArray = compressedString.split("");
+	var compressConversion = [];
+	for(i=0; i< compressedArray.length; i++) {
+		var ones,twos,fours,eights,sixteens,thirtytwos,character,value,temp;
+		character = compressedArray[i];
+		value = character.charCodeAt() - 33;
+		temp = value;
+		thirtytwos = Math.floor(value/32);
+		temp -= thirtytwos * 32;
+		sixteens = Math.floor(temp/16);
+		temp -= sixteens * 16;
+		eights = Math.floor(temp/8);
+		temp -= eights * 8;
+		fours = Math.floor(temp/4);
+		temp -= fours * 4;
+		twos = Math.floor(temp/2);
+		temp -= twos * 2;
+		ones = temp;
+		compressConversion.push(ones,twos,fours,eights,sixteens,thirtytwos);
+	}
+
+	return compressConversion;
 }
 
-var compressedArray = IW.stringConvert.split("");
-IW.compressConversion = [];
-for(i=0; i< compressedArray.length; i++) {
-	var ones,twos,fours,eights,sixteens,thirtytwos,character,value,temp;
-	character = compressedArray[i];
-	value = character.charCodeAt() - 33;
-	temp = value;
-	thirtytwos = Math.floor(value/32);
-	temp -= thirtytwos * 32;
-	sixteens = Math.floor(temp/16);
-	temp -= sixteens * 16;
-	eights = Math.floor(temp/8);
-	temp -= eights * 8;
-	fours = Math.floor(temp/4);
-	temp -= fours * 4;
-	twos = Math.floor(temp/2);
-	temp -= twos * 2;
-	ones = temp;
-	IW.compressConversion.push(ones,twos,fours,eights,sixteens,thirtytwos);
-}
+$.getJSON("/js/compressedDictionary.json",function(data) {
+	var compressedDictionary = data.dictionary;
+	IW.compressConversion = IW.extractArray(compressedDictionary);
+	console.log(compareArrays(IW.compressConversion));
+	
+})
 
-console.log(compareArrays(IW.compressConversion));
+// var storageObj = {};
+// storageObj.dictionary = compressedString;
+// fs.writeFile("compressedDictionary.txt", JSON.stringify(storageObj), function(err) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log("The file was saved!");
+//     }
+// }); 
